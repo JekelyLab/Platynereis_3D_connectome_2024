14b4a710bb203224fd8c310169e98934d16cf309
Gaspar Jekely <g.jekely@exeter.ac.uk>
2024-10-25 19:34:02 +0200

    Ms and fig, source data upload

--- supplements/generate_RGL_files_for_celltype_compendium_website.R
+++ supplements/generate_RGL_files_for_celltype_compendium_website.R
@@ -10,21 +10,15 @@ library(catmaid)
 library(rgl)
 library(htmltools)
 
-# catmaid connection
-#conn_http1 <- catmaid_login(
-#  server="https://catmaid.jekelylab.ex.ac.uk/", 
-#  authname="AnonymousUser",
-#  config=httr::config(ssl_verifypeer=0, http_version=1)
-#)
-
 # catmaid connection, needs username, password AND token - weird!
 {
   # can run this separate file using source function
   conn <- source("~/R/conn.R")
   #for larger calls we need to use http/1, see https://www.gitmemory.com/issue/natverse/rcatmaid/158/641537466
   #for this we configure to http/1.1
-#  conn_http1 = catmaid_login(conn=conn, config=httr::config(ssl_verifypeer=0, http_version=1))
+  conn_http1 = catmaid_login(conn=conn, config=httr::config(ssl_verifypeer=0, http_version=1))
 }
+
 
 
   
